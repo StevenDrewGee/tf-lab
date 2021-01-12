6d11093c326ffb0e822b912ad6cc7e124239bf48
StevenDrewGee <drughi.steven@gmail.com>
2021-01-12 11:13:56 -0500

    INITIAL setup, remove quoted variables, #1

--- config.tf
+++ config.tf
@@ -1,4 +1,4 @@
-### variables definition
+## variables definition
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 # variable "private_key_path" {}
@@ -6,8 +6,8 @@ variable "key_name" {default = "souza-key-use-1a"}
 
 ### providers definition
 provider "aws" {
-    access_key = "var.aws_access_key"
-    secret_key = "var.aws_secret_key"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
     region = "us-west-1"
 }
 
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami = "ami-09c5258a58aeabe14" # original class "ami-b2527ad2"
     instance_type = "t2.micro"
-    key_name = "var.key_name"
+    key_name = var.key_name
 }
 
 ### output definitions
