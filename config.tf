0204ee5ee6a99d132006e43a210ebb8b1b3dfbd1
StevenDrewGee <drughi.steven@gmail.com>
2021-01-12 17:02:42 +0000

    INITAIL setup correct region to east #1

--- config.tf
+++ config.tf
@@ -8,7 +8,7 @@ variable "key_name" {default = "souza-key-use-1a"}
 provider "aws" {
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
-    region = "us-west-1"
+    region = "us-east-1"
 }
 
 ### resources definition
