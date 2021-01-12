77c2cc59a644e7fecae1e2f2b1013b4e9ca72040
StevenDrewGee <drughi.steven@gmail.com>
2021-01-12 09:48:42 -0500

    INITIAL setup, correct Syntax errors #1

--- config.tf
+++ config.tf
@@ -1,13 +1,13 @@
 ### variables definition
-variable "aws_access key" {}
+variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {default = "souza-key-use-1a"}
 
 ### providers definition
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    access_key = "var.aws_access_key"
+    secret_key = "var.aws_secret_key"
     region = "us-west-1"
 }
 
@@ -15,10 +15,10 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami = "ami-09c5258a58aeabe14" # original class "ami-b2527ad2"
     instance_type = "t2.micro"
-    key_name = "${var.key_name}"
+    key_name = "var.key_name"
 }
 
 ### output definitions
 output "aws_instance_public_dns" {
-    value = "${aws_instance.webserver.public_dns}"
+    value = "aws_instance.webserver.public_dns"
 }
